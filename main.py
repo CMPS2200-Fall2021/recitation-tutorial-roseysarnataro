7c6c070530fb4f6d08fd8ed0dc81b5fa3c01a57f
roseysarnataro <70075361+roseysarnataro@users.noreply.github.com>
2021-08-26 09:36:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def sum_of_squares(a):
+	return sum([x**2 for x in a])
 	pass
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
-#comment
+
+def test_two():
+	assert sum_of_squares([2,3,4]) == 29
